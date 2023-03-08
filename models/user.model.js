222f1a4f1370277fd6e624a6f48eed54ba5f5493
kilendo-dingha-github <kilendodingha@rinabel.com>
2023-03-08 19:28:53 -0200

    m

--- models/user.model.js
+++ models/user.model.js
@@ -45,6 +45,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
     },
 	
+	descriptionAuteur: {
+      type: String,
+    },
+	
 	buttonActiverSurLeProfil: {
       type: String,
     },
