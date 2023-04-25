ada19a7ccaa76f00efa995f5e53e778169591c82
kilendo-dingha-github <kilendodingha@rinabel.com>
2023-04-25 11:18:11 -0200

    m

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -36,7 +36,7 @@ const createToken = (id) => {
 // inscription
 // inscription
 module.exports.signup = async (req, res) => {
-  const { nomAuteur, email, password, badgeVerified, utilisateur, buttonActiverSurLeProfil, visible } =
+  const { nomAuteur, email, password, badgeVerified, utilisateur, buttonActiverSurLeProfil, visible, photoProfil } =
     req.body;
   // on verifie en fonction de l'email si la personne ne s'etait pas deja inscrite auparavant
   const user = await userModel.findOne({ email }).exec();
@@ -58,7 +58,8 @@ module.exports.signup = async (req, res) => {
       badgeVerified,
       utilisateur,
       buttonActiverSurLeProfil, 
-	  visible
+	  visible,
+	  photoProfil,
     });
     res.status(200).json({
       message: "Inscription réussi avec succès ! ",
