c2af76f855aa4009ceb5f2d3f752e5f139ac34fa
kilendo <kilendodingha@gmail.com>
2023-11-10 17:31:51 -0400

    m

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -31,26 +31,6 @@ module.exports.allNotification = async (req, res) => {
   }
 };
 
-// one notification
-// one notification
-// en fonction de l'id, afficher une notification
-module.exports.oneNotification = async (req, res) => {
-  // on verifie si l'id envoyer dans la requete existe dans la bdd
-  // si l'id n'existe pas, on envoie ce message d'erreur
-  if (!ObjectID.isValid(req.params.id))
-    return res.status(400).send("ID introuvable : " + req.params.id);
-
-  try {
-    notificationModel.findById(req.params.id, (err, docs) => {
-      // dans ce cas, l'id existe, alors on affiche une notification
-      if (!err) res.status(200).json(docs);
-    });
-  } catch (err) {
-    // en cas d'echec, message d'erreur
-    return res.status(500).json(err);
-  }
-};
-
 // modifier la notification
 module.exports.updateNotification = async (req, res) => {
   // on verifie si l'id envoyer dans la requete existe dans la bdd
@@ -69,24 +49,3 @@ module.exports.updateNotification = async (req, res) => {
     return res.status(500).json(err);
   }
 };
-
-// supprimer une notification
-// supprimer une notification
-// ici c'est pour supprimer une notification
-module.exports.deleteNotification = async (req, res) => {
-  // on verifie si l'id envoyer dans la requete existe dans la bdd
-  // si l'id n'existe pas, on envoie ce message d'erreur
-  if (!ObjectID.isValid(req.params.id))
-    return res.status(400).send("ID introuvable : " + req.params.id);
-
-  try {
-    // id existe. on supprime la notification
-    await notificationModel.deleteOne({ _id: req.params.id }).exec();
-    // message de succès
-    res.status(200).json({ message: "notification supprimer avec succès" });
-  } catch (err) {
-    // en cas d'echec, message d'erreur
-    return res.status(500).json({ message: err });
-  }
-};
-// supprimer une notification
--- routes/notification.route.js
+++ routes/notification.route.js
@@ -2,16 +2,12 @@ const express = require("express");
 const {
   createNotification,
   allNotification,
-  oneNotification,
-  deleteNotification,
   updateNotification,
 } = require("../controllers/notification.controller");
 const notificationRouter = express.Router();
 
 notificationRouter.post("/create", createNotification);
 notificationRouter.get("/", allNotification);
-notificationRouter.get("/:id", oneNotification);
-notificationRouter.delete("/delete/:id", deleteNotification);
 notificationRouter.put("/update/:id", updateNotification);
 
 module.exports = notificationRouter;
